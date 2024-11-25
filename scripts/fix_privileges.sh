7aecbc6c86a65d83c10899f9b51b7c1649affffa
atola <atolagbe.elijah.21@gmail.com>
2024-11-25 03:51:38 +0100

    update spec files and scripts

---
+++ scripts/fix_privileges.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+chmod +x /home/ec2-user/server/* jar
+chmod +x /home/elijah/server/server_start.sh
+chmod +x /home/elijah/server/server_stop.sh
---
+++ scripts/server_start.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+cd /home/elijah/server
+sudo java -jar my-app.jar > /dev/null 2> /dev/null < /dev/null &
